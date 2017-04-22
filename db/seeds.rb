49bd8f60c4ad9227c66c7259d83edf5777e823a7
Jeff Brubaker <jtbrubak1@gmail.com>
2017-04-22 18:22:48 -0400

    Add artist page, minor touch-ups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tracks = [
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/01+-+d.s.+car+jam.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/02+-+theatre+is+the+life+of+you.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/03+-+viet+nam.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/04+-+cohesion.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/05+-+it\'s+expected+i\'m+gone.mp3',
+"https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/06+-+\#1+hit+song.mp3",
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/07+-+two+beads+at+the+end.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/08+-+do+you+want+new+wave+or+do+you+want+the+truth.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/09+-+don\'t+look+now.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/10+-+shit+from+an+old+notebook.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/11+-+nature+without+man.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/12+-+one+reporter\'s+opinion.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/13+-+political+song+for+michael+jackson+to+sing.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/14+-+maybe+partying+will+help.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/15+-+toadies.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/16+-+retreat.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/17+-+the+big+foist.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/18+-+god+bows+to+math.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/19+-+corona.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/20+-+the+glory+of+man.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/21+-+take+5+d.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/22+-+my+heart+and+the+real+world.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/23+-+history+lesson+-part2.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/24+-+you+need+the+glory.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/25+-+the+roar+of+the+masses+could+be+farts.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/26+-+west+germany.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/27+-+the+politics+of+time.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/28+-+themselves.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/29+-+please+don\'t+be+gentle+with+me.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/30+-+nothing+indeed.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/31+-+no+exchange.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/32+-+there+ain\'t+shit+on+tv+tonight.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/33+-+this+ain\'t+no+picnic.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/34+-+spillage.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/35+-+untitledsong+for+latin+america.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/36+-+jesus+and+tequila.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/37+-+june+16th.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/38+-+storm+in+my+house.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/39+-+martin\'s+story.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/40+-+dr.+wu.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/41+-+the+world+according+to+nouns.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/42+-+love+dance.mp3',
+'https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/43+-+three+car+jam.mp3'
+]
 
 User.destroy_all
 User.create(username: 'guest', password: 'password')
 
 Artist.destroy_all
 Artist.create(name: 'Black Flag', image: "https://s3.amazonaws.com/sstify-dev/seeds/artists/black_flag.jpg")
+Artist.create(name: 'Minutemen', image: "https://s3.amazonaws.com/sstify-dev/seeds/artists/minutemen.jpg")
+Artist.create(name: 'Sonic Youth', image: "https://s3.amazonaws.com/sstify-dev/seeds/artists/sonic_youth.jpg")
+Artist.create(name: 'Meat Puppets', image: "https://s3.amazonaws.com/sstify-dev/seeds/artists/")
 
 Album.destroy_all
 Album.create(artist: Artist.find_by_name('Black Flag'), title: 'My War', year: 1984, image: 'https://s3.amazonaws.com/sstify-dev/seeds/albums/my_war.jpg')
@@ -19,48 +60,6 @@ Album.create(artist: Artist.find_by_name('Minutemen'), title: "Double Nickels On
 Album.create(artist: Artist.find_by_name('Meat Puppets'), title: 'Meat Puppets II', year: 1984, image: 'https://s3.amazonaws.com/sstify-dev/seeds/albums/meat_puppets_ii.png')
 
 Track.destroy_all
-tracks = [
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/01+-+d.s.+car+jam.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/02+-+theatre+is+the+life+of+you.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/03+-+viet+nam.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/04+-+cohesion.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/05+-+it%27s+expected+i%27m+gone.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/06+-+%231+hit+song.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/07+-+two+beads+at+the+end.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/08+-+do+you+want+new+wave+or+do+you+want+the+truth.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/09+-+don%27t+look+now.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/10+-+shit+from+an+old+notebook.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/11+-+nature+without+man.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/12+-+one+reporter%27s+opinion.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/13+-+political+song+for+michael+jackson+to+sing.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/14+-+maybe+partying+will+help.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/15+-+toadies.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/16+-+retreat.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/17+-+the+big+foist.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/18+-+god+bows+to+math.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/19+-+corona.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/20+-+the+glory+of+man.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/21+-+take+5+d.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/22+-+my+heart+and+the+real+world.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/23+-+history+lesson+-part2.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/24+-+you+need+the+glory.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/25+-+the+roar+of+the+masses+could+be+farts.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/26+-+west+germany.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/27+-+the+politics+of+time.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/28+-+themselves.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/29+-+please+don%27t+be+gentle+with+me.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/30+-+nothing+indeed.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/31+-+no+exchange.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/32+-+there+ain%27t+shit+on+tv+tonight.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/33+-+this+ain%27t+no+picnic.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/34+-+spillage.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/35+-+untitledsong+for+latin+america.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/36+-+jesus+and+tequila.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/37+-+june+16th.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/38+-+storm+in+my+house.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/39+-+martin%27s+story.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/40+-+dr.+wu.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/41+-+the+world+according+to+nouns.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/42+-+love+dance.mp3
-https://s3.amazonaws.com/sstify-dev/seeds/tracks/1984+-+Double+Nickels+On+The+Dime/43+-+three+car+jam.mp3
-]
+tracks.each do |track|
+  Track.create(audio: track)
+end

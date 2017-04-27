969cff6803e2af35da0044cf4a2b254d196ba748
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-26 21:10:24 -0400

    Add playbar functionality

--- frontend/components/playbar/playbar.jsx
+++ frontend/components/playbar/playbar.jsx
@@ -4,10 +4,11 @@ class Playbar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { played: [], queue: [] };
+    this.state = { played: [], queue: [], status: 'pause' };
     this.nextTrack = this.nextTrack.bind(this);
     this.previousTrack = this.previousTrack.bind(this);
     this.pause = this.pause.bind(this);
+    this.play = this.play.bind(this);
   }
 
   currentTrack() {
@@ -19,25 +20,47 @@ class Playbar extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ played: nextProps.nowPlaying.played, queue: nextProps.nowPlaying.queue });
+    this.setState({ played: nextProps.nowPlaying.played, queue: nextProps.nowPlaying.queue, status: 'play' });
   }
 
   previousTrack() {
     const newQueue = this.state.queue;
     const newPlayed = this.state.played;
     newQueue.unshift(newPlayed.pop());
-    this.setState({ played: newPlayed, queue: newQueue });
+    this.setState({ played: newPlayed, queue: newQueue, status: 'play' });
   }
 
   nextTrack() {
     const newQueue = this.state.queue;
     const newPlayed = this.state.played;
     newPlayed.push(newQueue.shift());
-    this.setState({ played: newPlayed, queue: newQueue });
+    this.setState({ played: newPlayed, queue: newQueue, status: 'play' });
   }
 
   pause() {
     this.audio.pause();
+    this.setState({ status: 'pause' });
+  }
+
+  play() {
+    this.audio.play();
+    this.setState({ status: 'play' });
+  }
+
+  pauseStatus() {
+    if (this.state.status === 'play') {
+      return "material-icons";
+    } else {
+      return "hidden";
+    }
+  }
+
+  playStatus() {
+    if (this.state.status === 'play') {
+      return "hidden";
+    } else {
+      return "material-icons";
+    }
   }
 
   render() {
@@ -48,7 +71,8 @@ class Playbar extends React.Component {
         </audio>
         <div className="play-controls">
           <i onClick={this.previousTrack} className="material-icons">skip_previous</i>
-          <i onClick={this.pause} className="material-icons">play_circle_outline</i>
+          <i id="audio-play-button" onClick={this.play} className={this.playStatus()}>play_circle_outline</i>
+          <i id="audio-pause-button" onClick={this.pause} className={this.pauseStatus()}>pause_circle_outline</i>
           <i onClick={this.nextTrack} className="material-icons">skip_next</i>
         </div>
       </div>

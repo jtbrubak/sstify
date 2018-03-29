85c33e972324c041d82b64eec6b16c280294101b
Jeff Brubaker <jtbrubak1@gmail.com>
2018-03-28 23:05:11 -0400

    Add window cleanup

--- frontend/sstify.jsx
+++ frontend/sstify.jsx
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
+    delete window.currentUser;
   } else {
     store = configureStore();
   }
